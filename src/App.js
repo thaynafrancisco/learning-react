32f8d1a376442e3e9f2730d3dd06e521c292522b
Thayna Francisco <thayna.francisco@global.ntt>
2022-01-17 18:04:51 -0300

    aula 4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import FormularioCadastro from './components/FormularioCadastro';
 import ListaDeNotas from './components/ListaDeNotas';
+import "./Assets/App.css"
+import "./Assets/index.css"
+
 
 class App extends Component {
   render(){
     return (
-      <section>
+      <div className="conteudo">
       <FormularioCadastro/>
       <ListaDeNotas/>
-      </section>
+      </div>
     );
   }
 }
